ecc915531388d4c6951dd12edb74f38f2ba15f2d
Ray Zhang <ray@ellipsislabs.xyz>
2025-07-06 22:27:47 -0400

    stash

--- src/lib.rs
+++ src/lib.rs
@@ -38,11 +38,9 @@ mod tests {
                 let mut reader = reader_factory.clone();
                 let randomized_input = randomized_input.clone();
                 std::thread::spawn(move || {
-                    let mut cursor = 0;
                     let mut result = Vec::new();
-                    while cursor < size {
+                    while !reader.is_finished() {
                         while let Some(value) = reader.next() {
-                            cursor += 1;
                             result.push(value.clone());
                         }
                         std::thread::yield_now();
@@ -66,8 +64,8 @@ mod tests {
 
     #[test]
     fn two_producers_two_consumers() {
-        let size = 10000;
-        let ring_buffer = Box::leak(Box::new(MPRingBuffer::<u32, 1024>::new()));
+        let size = 5000;
+        let ring_buffer = Box::leak(Box::new(MPRingBuffer::<u32, 2048>::new()));
         let (reader_factory, writer_factory) = ring_buffer.split();
         let input1 = (0..size).map(|_| rand::random::<u32>()).collect::<Vec<_>>();
         let input2 = (0..size).map(|_| rand::random::<u32>()).collect::<Vec<_>>();
@@ -82,6 +80,7 @@ mod tests {
         let mut inputs = vec![input1, input2];
 
         println!("Starting test with size: {size}");
+
         // Spawn writer thread
         let writer_handle = (0..2)
             .map(|_| {
@@ -101,17 +100,18 @@ mod tests {
             })
             .collect::<Vec<_>>();
 
+        // otherwise the extra writer will block the readers from finishing
+        drop(writer_factory);
+
         // Spawn reader threads
         let reader_handles = (0..10)
             .map(|i| {
                 let mut reader = reader_factory.clone();
                 let final_result = final_result.clone();
                 std::thread::spawn(move || {
-                    let mut cursor = 0;
                     let mut result = Vec::new();
-                    while cursor < size * 2 {
+                    while !reader.is_finished() {
                         while let Some(value) = reader.next() {
-                            cursor += 1;
                             result.push(value.clone());
                         }
                         std::thread::yield_now();
@@ -139,5 +139,135 @@ mod tests {
         }
 
         println!("Done");
+    }
+
+    #[test]
+    fn mpsc_counter_stress_test() {
+        use std::sync::Arc;
+        use std::sync::atomic::{AtomicI32, Ordering};
+
+        const MAX_EVENTS: usize = 1_000_000;
+        const BUF_SIZE: usize = 32768;
+
+        let ring_buffer = Box::leak(Box::new(MPRingBuffer::<i32, BUF_SIZE>::new()));
+        let (mut rx, mut tx) = ring_buffer.split();
+        let mut tx2 = tx.clone();
+
+        // Producer 1
+        let t1 = std::thread::spawn(move || {
+            for _ in 0..MAX_EVENTS {
+                tx.put(1);
+            }
+            tx.mark_as_finished();
+        });
+
+        // Producer 2
+        let t2 = std::thread::spawn(move || {
+            for _ in 0..MAX_EVENTS {
+                tx2.put(1);
+            }
+            tx2.mark_as_finished();
+        });
+
+        let sink = Arc::new(AtomicI32::new(0));
+        let sink_clone = Arc::clone(&sink);
+
+        // Consumer
+        let c1 = std::thread::spawn(move || {
+            while !rx.is_finished() {
+                while let Some(value) = rx.next() {
+                    sink_clone.fetch_add(*value, Ordering::Release);
+                }
+            }
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+        c1.join().unwrap();
+
+        let final_count = sink.load(Ordering::Acquire) as usize;
+        assert_eq!(
+            final_count,
+            MAX_EVENTS * 2,
+            "Expected {}, got {}",
+            MAX_EVENTS * 2,
+            final_count
+        );
+    }
+
+    #[test]
+    fn mpsc_small_buffer_test() {
+        use std::sync::Arc;
+        use std::sync::atomic::{AtomicI32, AtomicUsize, Ordering};
+
+        // Use a tiny buffer to force wrapping - same scale as failing test
+        const MAX_EVENTS: usize = 1_000_000;
+        const BUF_SIZE: usize = 64;
+
+        let ring_buffer = Box::leak(Box::new(MPRingBuffer::<i32, BUF_SIZE>::new()));
+        let (mut rx, mut tx) = ring_buffer.split();
+        let mut tx2 = tx.clone();
+
+        let write_count = Arc::new(AtomicUsize::new(0));
+        let write_count1 = write_count.clone();
+        let write_count2 = write_count.clone();
+
+        // Producer 1
+        let t1 = std::thread::spawn(move || {
+            for i in 0..MAX_EVENTS {
+                tx.put(1);
+                write_count1.fetch_add(1, Ordering::Release);
+                if i % 10 == 0 {
+                    std::thread::yield_now();
+                }
+            }
+            tx.mark_as_finished();
+        });
+
+        // Producer 2
+        let t2 = std::thread::spawn(move || {
+            for i in 0..MAX_EVENTS {
+                tx2.put(1);
+                write_count2.fetch_add(1, Ordering::Release);
+                if i % 10 == 0 {
+                    std::thread::yield_now();
+                }
+            }
+            tx2.mark_as_finished();
+        });
+
+        let sink = Arc::new(AtomicI32::new(0));
+        let sink_clone = Arc::clone(&sink);
+
+        // Consumer
+        let c1 = std::thread::spawn(move || {
+            while !rx.is_finished() {
+                while let Some(value) = rx.next() {
+                    sink_clone.fetch_add(*value, Ordering::Release);
+                }
+            }
+        });
+
+        t1.join().unwrap();
+        t2.join().unwrap();
+        c1.join().unwrap();
+
+        let total_written = write_count.load(Ordering::Acquire);
+        let final_count = sink.load(Ordering::Acquire) as usize;
+
+        println!(
+            "Small buffer test: written={}, read={}, lost={}",
+            total_written,
+            final_count,
+            total_written - final_count
+        );
+
+        // With a small buffer, we expect some loss due to overwriting
+        assert!(
+            final_count <= total_written,
+            "Read more than written! read={}, written={}",
+            final_count,
+            total_written
+        );
     }
 }
--- src/ringbuffer.rs
+++ src/ringbuffer.rs
@@ -1,4 +1,8 @@
-use std::{cell::Cell, marker::PhantomData, sync::atomic::AtomicUsize};
+use std::{
+    cell::Cell,
+    marker::PhantomData,
+    sync::atomic::{AtomicBool, AtomicUsize},
+};
 
 use crate::valuebox::ValueBox;
 
@@ -8,6 +12,9 @@ use crate::valuebox::ValueBox;
 pub trait RBContainer<T: Sized + Clone>: Sized {
     fn retrieve<'a>(&'a self, cursor: usize) -> Option<&'a T>;
     fn put(&self, value: T);
+    fn mark_as_finished(&self);
+    fn is_finished(&self) -> bool;
+    fn next_readable(&self) -> usize;
 }
 
 /// RingBuffer used for single producer scenario
@@ -15,6 +22,7 @@ pub struct SPRingBuffer<T: Sized + Clone, const N: usize> {
     data: [ValueBox<T>; N],
     next_write: Cell<usize>,
     next_readable: AtomicUsize,
+    is_finished: AtomicBool,
 }
 
 impl<T: Sized + Clone, const N: usize> SPRingBuffer<T, N> {
@@ -23,6 +31,7 @@ impl<T: Sized + Clone, const N: usize> SPRingBuffer<T, N> {
             data: core::array::from_fn(|_| ValueBox::uninit()),
             next_write: Cell::new(0),
             next_readable: AtomicUsize::new(0),
+            is_finished: AtomicBool::new(false),
         }
     }
 
@@ -33,7 +42,10 @@ impl<T: Sized + Clone, const N: usize> SPRingBuffer<T, N> {
                 cursor: 0,
                 _marker: PhantomData,
             },
-            RBWriter { ring_buffer: self },
+            RBWriter {
+                ring_buffer: self,
+                finished: false,
+            },
         )
     }
 }
@@ -70,6 +82,23 @@ impl<T: Sized + Clone, const N: usize> RBContainer<T> for SPRingBuffer<T, N> {
                 .fetch_add(1, std::sync::atomic::Ordering::Release);
         }
     }
+
+    #[inline(always)]
+    fn mark_as_finished(&self) {
+        self.is_finished
+            .store(true, std::sync::atomic::Ordering::Release);
+    }
+
+    #[inline(always)]
+    fn is_finished(&self) -> bool {
+        self.is_finished.load(std::sync::atomic::Ordering::Acquire)
+    }
+
+    #[inline(always)]
+    fn next_readable(&self) -> usize {
+        self.next_readable
+            .load(std::sync::atomic::Ordering::Acquire)
+    }
 }
 
 /// RingBuffer used for multi producer scenario
@@ -77,6 +106,8 @@ pub struct MPRingBuffer<T: Sized + Clone, const N: usize> {
     data: [ValueBox<T>; N],
     next_write: AtomicUsize,
     next_readable: AtomicUsize,
+    producer_count: AtomicUsize,
+    finish_count: AtomicUsize,
 }
 impl<T: Sized + Clone, const N: usize> MPRingBuffer<T, N> {
     pub fn new() -> Self {
@@ -84,17 +115,24 @@ impl<T: Sized + Clone, const N: usize> MPRingBuffer<T, N> {
             data: core::array::from_fn(|_| ValueBox::uninit()),
             next_write: AtomicUsize::new(0),
             next_readable: AtomicUsize::new(0),
+            producer_count: AtomicUsize::new(0),
+            finish_count: AtomicUsize::new(0),
         }
     }
 
     pub fn split(&self) -> (RBReader<T, Self>, RBWriterClonable<T, N>) {
+        self.producer_count
+            .fetch_add(1, std::sync::atomic::Ordering::Release);
         (
             RBReader {
                 ring_buffer: self,
                 cursor: 0,
                 _marker: PhantomData,
             },
-            RBWriterClonable { ring_buffer: self },
+            RBWriterClonable {
+                ring_buffer: self,
+                finished: false,
+            },
         )
     }
 }
@@ -133,6 +171,27 @@ impl<T: Sized + Clone, const N: usize> RBContainer<T> for MPRingBuffer<T, N> {
         self.next_readable
             .fetch_add(1, std::sync::atomic::Ordering::Release);
     }
+
+    #[inline(always)]
+    fn mark_as_finished(&self) {
+        self.finish_count
+            .fetch_add(1, std::sync::atomic::Ordering::Release);
+    }
+
+    #[inline(always)]
+    fn is_finished(&self) -> bool {
+        let producer_count = self
+            .producer_count
+            .load(std::sync::atomic::Ordering::Relaxed);
+        let finish_count = self.finish_count.load(std::sync::atomic::Ordering::Acquire);
+        finish_count == producer_count
+    }
+
+    #[inline(always)]
+    fn next_readable(&self) -> usize {
+        self.next_readable
+            .load(std::sync::atomic::Ordering::Acquire)
+    }
 }
 
 /// RingBuffer reader, cloneable, supports multiple concurrent readers maintaining their own cursor
@@ -161,11 +220,17 @@ impl<'a, T: Sized + Clone, C: RBContainer<T>> RBReader<'a, T, C> {
             v
         })
     }
+
+    #[inline(always)]
+    pub fn is_finished(&self) -> bool {
+        self.ring_buffer.is_finished() && self.cursor == self.ring_buffer.next_readable()
+    }
 }
 
 /// RingBuffer writer, non-cloneable, supports single writer
 pub struct RBWriter<'a, T: Sized + Clone, const N: usize> {
     ring_buffer: &'a SPRingBuffer<T, N>,
+    finished: bool,
 }
 
 unsafe impl<'a, T: Sized + Clone, const N: usize> Send for RBWriter<'a, T, N> {}
@@ -175,12 +240,26 @@ impl<'a, T: Sized + Clone, const N: usize> RBWriter<'a, T, N> {
     pub fn put(&mut self, value: T) {
         self.ring_buffer.put(value);
     }
+
+    #[inline(always)]
+    pub fn mark_as_finished(&mut self) {
+        if !self.finished {
+            self.finished = true;
+            self.ring_buffer.mark_as_finished();
+        }
+    }
+}
+
+impl<T: Sized + Clone, const N: usize> Drop for RBWriter<'_, T, N> {
+    fn drop(&mut self) {
+        self.mark_as_finished();
+    }
 }
 
 /// RingBuffer writer, cloneable, supports multiple concurrent writers
-#[derive(Clone)]
 pub struct RBWriterClonable<'a, T: Sized + Clone, const N: usize> {
     ring_buffer: &'a MPRingBuffer<T, N>,
+    finished: bool,
 }
 
 unsafe impl<'a, T: Sized + Clone, const N: usize> Send for RBWriterClonable<'a, T, N> {}
@@ -189,5 +268,31 @@ impl<'a, T: Sized + Clone, const N: usize> RBWriterClonable<'a, T, N> {
     #[inline(always)]
     pub fn put(&mut self, value: T) {
         self.ring_buffer.put(value);
+    }
+
+    #[inline(always)]
+    pub fn mark_as_finished(&mut self) {
+        if !self.finished {
+            self.finished = true;
+            self.ring_buffer.mark_as_finished();
+        }
+    }
+}
+
+impl<T: Sized + Clone, const N: usize> Drop for RBWriterClonable<'_, T, N> {
+    fn drop(&mut self) {
+        self.mark_as_finished();
+    }
+}
+
+impl<T: Sized + Clone, const N: usize> Clone for RBWriterClonable<'_, T, N> {
+    fn clone(&self) -> Self {
+        self.ring_buffer
+            .producer_count
+            .fetch_add(1, std::sync::atomic::Ordering::Release);
+        Self {
+            ring_buffer: self.ring_buffer,
+            finished: false,
+        }
     }
 }
